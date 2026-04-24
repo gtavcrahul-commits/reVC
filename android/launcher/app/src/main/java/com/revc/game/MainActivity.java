4022788e0c2f05c3d343e9dd63d60f00ea95e8ea
gtavcrahul-commits <gtavcrahul@gmail.com>
2026-04-24 19:38:58 +0600

    Uncomment library loading in MainActivity

--- android/launcher/app/src/main/java/com/revc/game/MainActivity.java
+++ android/launcher/app/src/main/java/com/revc/game/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'game' library on application startup.
     static {
-        //System.loadLibrary("revc");
+        System.loadLibrary("revc");
     }
 
     private ActivityMainBinding binding;
